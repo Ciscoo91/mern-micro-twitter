ee48cf31f2457e7312b6f13f261615d9c6fcbd10
Ciscoo91 <franciscomambo91@gmail.com>
2020-08-11 02:49:04 +0200

    log error from app.js on error

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.json('error');
+  res.json({ "error": err.message });
 });
 
 // Redirect all the routes to client/build/index.html
