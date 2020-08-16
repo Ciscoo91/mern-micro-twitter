ab60c442d91d94ce805ded31ef5383df2104f82b
Ciscoo91 <franciscomambo91@gmail.com>
2020-08-17 00:57:27 +0200

    test: updated the file to send in index route to avoir 404 error after deploy

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(function (req, res, next) {
   next();
 });
 
-// app.use('/', indexRouter);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/messages', messagesRouter);
 app.use('/avatar', avatarRouter);
